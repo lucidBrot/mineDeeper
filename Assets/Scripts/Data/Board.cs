fd4739fc4ea0fa0fff09ca564f1246842f4a5115
Jasper Ermatinger <chillersanim@gmail.com>
2019-10-11 23:05:50 +0200

    Added board and boardCell classes.

---
+++ Assets/Scripts/Data/Board.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board
+{
+    public int Width { get; }
+
+    public int Height { get; }
+
+    public int Depth { get; }
+
+    public BoardCell[,,] Cells { get; }
+
+    public int BombCount { get; set; }
+
+    public Board(int width, int height, int depth)
+    {
+        this.Width = width;
+        this.Height = height;
+        this.Depth = depth;
+
+        this.Cells = new BoardCell[width, height, depth];
+
+        for (var x = 0; x < width; x++)
+        {
+            for (var y = 0; y < height; y++)
+            {
+                for (var z = 0; z < depth; z++)
+                {
+                    this.Cells[x, y, z] = new BoardCell(x, y, z);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Data/BoardCell.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardCell
+{
+    public int PosX { get; }
+
+    public int PosY { get; }
+
+    public int PosZ { get; }
+
+    public int AdjacentBombCount { get; set; }
+
+    public bool IsBomb { get; set; }
+
+    public bool IsNude => AdjacentBombCount == 0;
+
+    public bool IsRevealed { get; set; }
+
+    public bool IsSuspect { get; set; }
+
+    public BoardCell(int posX, int posY, int posZ)
+    {
+        this.PosX = posX;
+        this.PosY = posY;
+        this.PosZ = posZ;
+    }
+}
