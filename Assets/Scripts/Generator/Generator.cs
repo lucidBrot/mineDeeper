1b20d17186b7c9f9105c9ab2290b06398e8718f4
U-FRIENDLY_NAME\eric <eric@mink.li>
2019-10-12 00:04:21 +0200

    prepare to pull

--- Assets/Scripts/Generator/Generator.cs
+++ Assets/Scripts/Generator/Generator.cs
@@ -8,16 +8,27 @@ namespace Assets.Scripts.Generator
 {
     public class Generator
     {
+        private Random random;
         public Generator ()
         {
-
+            random = new Random(1337);
         }
 
-        public Board generate (int boardWidth, int boardHeight, int boardDepth, int numBombs)
+        public Board Generate (int boardWidth, int boardHeight, int boardDepth, int numBombs)
         {
             Board board = new Board(boardWidth, boardHeight, boardDepth);
+
+            for (int bombCount = 0; bombCount < numBombs; bombCount++)
+            {
+                PlaceBombOnBoard(board);
+            }
+
             return board;
         }
 
+        private void PlaceBombOnBoard(Board board)
+        {
+            
+        }
     }
 }
