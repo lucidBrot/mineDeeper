d39629cc80deb35d42ff7e582b9adee0c5cd2983
U-FRIENDLY_NAME\eric <eric@mink.li>
2019-10-11 23:48:29 +0200

    Generator

---
+++ Assets/Scripts/Generator/Generator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Generator
+{
+    public class Generator
+    {
+        public Generator ()
+        {
+
+        }
+
+        public Board generate (int boardWidth, int boardHeight, int boardDepth, int numBombs)
+        {
+            Board board = new Board(boardWidth, boardHeight, boardDepth);
+            return board;
+        }
+
+    }
+}
