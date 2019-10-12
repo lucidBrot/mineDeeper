4d3ab9f14b61d8949af1cf98dbb2a5a4e1f5efe9
Jasper Ermatinger <chillersanim@gmail.com>
2019-10-12 02:01:23 +0200

    Added shit like textures and a field visualizer.

--- Assets/Scripts/Frontend/FieldVisual.cs
+++ Assets/Scripts/Frontend/FieldVisual.cs
@@ -1,18 +1,84 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
+using TMPro;
 using UnityEngine;
 
 public class FieldVisual : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject TextContainer;
+
+    public GameObject FlagContainer;
+
+    public GameObject CubeVisual;
+
+    public GameObject OutlineVisual;
+
+    private BoardCell boardCell;
+
+    public BoardCell BoardCell
     {
-        
+        get => boardCell;
+        set
+        {
+            if (boardCell == value)
+            {
+                return;
+            }
+
+            if (boardCell != null)
+            {
+                boardCell.PropertyChanged -= OnBoardCellPropertyChanged;
+            }
+
+            boardCell = value;
+
+            if (boardCell != null)
+            {
+                boardCell.PropertyChanged += OnBoardCellPropertyChanged;
+                UpdateVisual();
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnBoardCellPropertyChanged(object sender, PropertyChangedEventArgs e)
     {
-        
+        UpdateVisual();
+    }
+
+    private void UpdateVisual()
+    {
+        if (boardCell == null)
+        {
+            return;
+        }
+
+        if (FlagContainer != null)
+        {
+            FlagContainer.SetActive(!boardCell.IsRevealed && boardCell.IsSuspect);
+        }
+
+        if (CubeVisual != null)
+        {
+            CubeVisual.SetActive(!boardCell.IsRevealed);
+        }
+
+        if (OutlineVisual != null)
+        {
+            OutlineVisual.SetActive(boardCell.IsRevealed && !boardCell.IsNude);
+        }
+
+        if (TextContainer != null)
+        {
+            TextContainer.SetActive(boardCell.IsRevealed && !boardCell.IsNude);
+
+            var text = boardCell.IsNude ? string.Empty : boardCell.AdjacentBombCount.ToString();
+            var textObjects = TextContainer.GetComponentsInChildren<TextMeshPro>();
+
+            foreach (var textObject in textObjects)
+            {
+                textObject.text = text;
+            }
+        }
     }
 }
