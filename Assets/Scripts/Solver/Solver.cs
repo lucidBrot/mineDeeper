5a2a608fff977ea12d8e77ff593e1f8a6e8034c1
U-FRIENDLY_NAME\eric <eric@mink.li>
2019-10-12 02:25:11 +0200

    consider all neighbors are bombs

--- Assets/Scripts/Solver/Solver.cs
+++ Assets/Scripts/Solver/Solver.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
 using UnityEngine;
 
 namespace Assets.Scripts.Solver
@@ -10,6 +12,7 @@ namespace Assets.Scripts.Solver
     class Solver
     {
         private readonly Board board;
+        private const int NUM_NEIGHBORS = 26;
         private bool? solvable;
 
         public Solver(Board board)
@@ -29,7 +32,39 @@ namespace Assets.Scripts.Solver
 
         private void Compute()
         {
-            throw new NotImplementedException();
+            foreach (BoardCell cell in this.board.Cells)
+            {
+                ConsiderAllNeighborsAreBombs(cell);
+                // TODO: consider no neighbors are bombs
+            }
+        }
+
+        private void ConsiderAllNeighborsAreBombs(BoardCell cell)
+        {
+            // if the number of adjacent uncertainties equals the number on the cell, every uncertainty is a bomb
+            int revealedSafeCells = 0;
+            List<BoardCell> possibleBombs = new List<BoardCell>();
+            foreach (BoardCell neighbor in board.GetAdjacentCells(cell.PosX, cell.PosY, cell.PosZ))
+            {
+                if (neighbor.IsRevealed && !neighbor.IsBomb)
+                {
+                    revealedSafeCells++;
+                }
+
+                if (!neighbor.IsRevealed)
+                {
+                    possibleBombs.Add(neighbor);
+                }
+            }
+
+            if (revealedSafeCells + cell.AdjacentBombCount == NUM_NEIGHBORS)
+            {
+                // they are all bombs
+                foreach (BoardCell bomb in possibleBombs)
+                {
+                    bomb.IsSuspect = true;
+                }
+            }
         }
     }
 }
