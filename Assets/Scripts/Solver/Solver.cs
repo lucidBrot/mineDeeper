9259c541b42069fedbe91e6cd972c653f2036d31
U-FRIENDLY_NAME\eric <eric@mink.li>
2020-02-15 17:17:45 +0100

    Compare hints using IsEquivalentTo() with a HintType enum and a target cell.

--- Assets/Scripts/Solver/Solver.cs
+++ Assets/Scripts/Solver/Solver.cs
@@ -76,29 +76,33 @@ namespace Assets.Scripts.Solver
                 computationAdvancedThisTurn |= solver.ConsiderAllHiddenNeighborsAreBombs(cell, modifyBoard:false);
                 if (computationAdvancedThisTurn)
                 {
-                    return new Hint("Consider that all hidden neighbors of "+cell.ToString()+" are bombs.", cell);
+                    return new Hint(
+                        cell, Data.Hint.HintTypes.AllHiddenNeighborsAreBombs,
+                        "Consider that all hidden neighbors of "+cell.ToString()+" are bombs.", cell);
                 }
 
                 computationAdvancedThisTurn |= solver.ConsiderAllNeighborsAreSafe(cell, modifyBoard: false);
                 if (computationAdvancedThisTurn)
                 {
-                    return new Hint("Consider that all neighbors of " + cell.ToString() + " are certainly safe.", cell);
+                    return new Hint(cell, Data.Hint.HintTypes.AllNeighborsAreSafe, 
+                        "Consider that all neighbors of " + cell.ToString() + " are certainly safe.", cell);
                 }
 
                 computationAdvancedThisTurn |= solver.ConsiderTheLackOfRemainingAdjacentBombs(cell, modifyBoard: false);
                 if (computationAdvancedThisTurn)
                 {
-                    return new Hint("Consider that there can not be any more bombs around " + cell.ToString() + " than you already found.", cell);
+                    return new Hint(cell, Data.Hint.HintTypes.MaxAdjacentBombsReached, 
+                        "Consider that there can not be any more bombs around " + cell.ToString() + " than you already found.", cell);
                 }
                 // TODO: Need to modify this code whenever the solver.Compute function is modified. Bad.
             }
 
             if (solver.numUnfoundBombs == 0)
             {
-                return new Hint("Won.", new List<BoardCell>());
+                return new Hint(null, Data.Hint.HintTypes.GameWon, "Won.", new List<BoardCell>());
             }
 
-            return new Hint("Look, I'm bamboozled. We're stuck", new List<BoardCell>());
+            return new Hint(null, Data.Hint.HintTypes.Bamboozled, "Look, I'm bamboozled. We're stuck", new List<BoardCell>());
         }
 
         /// <summary>
@@ -117,7 +121,8 @@ namespace Assets.Scripts.Solver
                 if (revealedNeighbor.AdjacentBombCount < numFlagsAroundRevealedNeighbor)
                 {
                     // there must be a wrong flag there
-                    return new Hint("Too many flags around " + revealedNeighbor.ToString(), revealedNeighbor);
+                    return new Hint(revealedNeighbor, Data.Hint.HintTypes.UserPlacedTooManyFlagsHere, 
+                        "Too many flags around " + revealedNeighbor.ToString(), revealedNeighbor);
                 }
             }
 
