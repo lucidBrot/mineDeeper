f1fe281d6ee76b8ff81206290a503c768789a41b
chillersanim <chillersanim@gmail.com>
2020-07-17 13:21:04 +0200

    Added keycode detector to debug cross device issues with input.

---
+++ Assets/Scripts/GameLogic/KeyDetector.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class KeyDetector : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (var keycode in Enum.GetValues(typeof(KeyCode)).OfType<KeyCode>())
+        {
+            if (Input.GetKeyDown(keycode))
+            {
+                this.GetComponent<Text>().text = keycode.ToString();
+            }
+        }
+    }
+}
